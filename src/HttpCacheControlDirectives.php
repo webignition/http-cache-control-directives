8572f4f078ed678b489fb6022e4671d71bd766ca
Jon Cram <jon@webignition.net>
2018-10-18 12:50:08 +0100

    Parse directives

--- src/HttpCacheControlDirectives.php
+++ src/HttpCacheControlDirectives.php
@@ -14,8 +14,43 @@ class HttpCacheControlDirectives
      */
     private $directives = [];
 
+    public function __construct(string $directives = '')
+    {
+        $this->directives = $this->parse($directives);
+    }
+
     public function getDirectives(): array
     {
         return $this->directives;
+    }
+
+    private function parse(string $directivesString): array
+    {
+        $directives = [];
+
+        $directivesString = trim($directivesString);
+
+        if (empty($directivesString)) {
+            return $directives;
+        }
+
+        $directiveStrings = explode(' ', $directivesString);
+
+        foreach ($directiveStrings as $directiveString) {
+            $directiveString = trim($directiveString);
+
+            if (!empty($directiveString)) {
+                $directiveParts = explode('=', $directiveString);
+
+                $token = strtolower($directiveParts[0]);
+                $value = $directiveParts[1] ?? null;
+
+                $directives[$token] = $value;
+            }
+        }
+
+        asort($directives);
+
+        return $directives;
     }
 }

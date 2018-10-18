b700b78a9478522d38a00e07c0c0ed05e6422848
Jon Cram <jon@webignition.net>
2018-10-18 16:19:11 +0100

    Parse directives with respect to value type

--- src/HttpCacheControlDirectives.php
+++ src/HttpCacheControlDirectives.php
@@ -9,6 +9,40 @@ namespace webignition\HttpCacheControlDirectives;
  */
 class HttpCacheControlDirectives
 {
+    const MAX_AGE = 'max-age';
+    const MAX_STALE = 'max-stale';
+    const MIN_FRESH = 'min-fresh';
+    const NO_CACHE = 'no-cache';
+    const NO_STORE = 'no-store';
+    const NO_TRANSFORM = 'no-transform';
+    const ONLY_IF_CACHED = 'only-if-cached';
+    const MUST_REVALIDATE = 'must-revalidate';
+    const PUBLIC = 'public';
+    const PRIVATE = 'private';
+    const PROXY_REVALIDATE = 'proxy-revalidate';
+    const S_MAXAGE = 's-maxage';
+
+    const TYPE_INT = 'int';
+    const TYPE_NONE = 'none';
+
+    /**
+     * @var array
+     */
+    private $directiveValueTypes = [
+        self::MAX_AGE => self::TYPE_INT,
+        self::MAX_STALE => self::TYPE_INT,
+        self::MIN_FRESH => self::TYPE_INT,
+        self::NO_CACHE => self::TYPE_NONE,
+        self::NO_STORE => self::TYPE_NONE,
+        self::NO_TRANSFORM => self::TYPE_NONE,
+        self::ONLY_IF_CACHED => self::TYPE_NONE,
+        self::MUST_REVALIDATE => self::TYPE_NONE,
+        self::PUBLIC => self::TYPE_NONE,
+        self::PRIVATE => self::TYPE_NONE,
+        self::PROXY_REVALIDATE => self::TYPE_NONE,
+        self::S_MAXAGE => self::TYPE_INT,
+    ];
+
     /**
      * @var array
      */
@@ -45,6 +79,16 @@ class HttpCacheControlDirectives
                 $token = strtolower($directiveParts[0]);
                 $value = $directiveParts[1] ?? null;
 
+                if (isset($this->directiveValueTypes[$token])) {
+                    if (self::TYPE_INT === $this->directiveValueTypes[$token]) {
+                        $value = (int) $value;
+                    }
+
+                    if (self::TYPE_NONE === $this->directiveValueTypes[$token]) {
+                        $value = null;
+                    }
+                }
+
                 $directives[$token] = $value;
             }
         }
@@ -53,4 +97,6 @@ class HttpCacheControlDirectives
 
         return $directives;
     }
+
+
 }
